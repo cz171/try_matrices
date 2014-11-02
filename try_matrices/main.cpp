38a52e384642a7bf6493ca7f65f964870bc9a44c
展陈 <zhanchen@zhanchens-MBP.home>
2014-11-01 22:18:18 -0400

    Initial Commit

---
+++ try_matrices/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  try_matrices
+//
+//  Created by 展陈 on 11/1/14.
+//  Copyright (c) 2014 Zhan CHEN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
